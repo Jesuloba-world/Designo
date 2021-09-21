dea41f22e15c7ca9fbf31afedc6f3823ac0a8e29
Jesuloba <jesulobajohn@gmail.com>
2021-09-21 04:19:07 +0100

    location responsive

--- src/components/Location/style/location.ts
+++ src/components/Location/style/location.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/macro";
 import { Heading2, Body } from "../../UI/typography/style/typo";
+import { devices } from "../../devices";
 
 export interface place {
 	reverse?: boolean;
@@ -12,6 +13,20 @@ export const Container = styled.div`
 	display: grid;
 	grid-template-rows: repeat(3, 32.6rem);
 	row-gap: 3.2rem;
+
+	@media ${devices.tablet} {
+		margin-bottom: 12rem;
+		row-gap: 12rem;
+		grid-template-rows: repeat(3, 67.6rem);
+	}
+
+	@media ${devices.tablet700} {
+		row-gap: 4rem;
+	}
+
+	@media ${devices.tablet600} {
+		grid-template-rows: repeat(3, 71.4rem);
+	}
 `;
 
 export const Place = styled.div<place>`
@@ -20,6 +35,20 @@ export const Place = styled.div<place>`
 	column-gap: 3rem;
 	grid-template-areas: ${({ reverse }) =>
 		reverse ? `"map frame frame"` : `"frame frame map"`};
+
+	@media ${devices.tablet} {
+		grid-template-columns: 100%;
+		grid-template-rows: repeat(2, 1fr);
+		row-gap: 2.4rem;
+	}
+
+	@media ${devices.tablet700} {
+		row-gap: 0;
+	}
+
+	@media ${devices.tablet600} {
+		grid-template-rows: 32rem auto;
+	}
 `;
 
 export const Frame = styled.div`
@@ -31,6 +60,15 @@ export const Frame = styled.div`
 	align-items: flex-end;
 	justify-content: flex-start;
 	position: relative;
+
+	@media ${devices.tablet} {
+		grid-row: 2 / span 1;
+		grid-column: 1 / span 1;
+	}
+
+	@media ${devices.tablet700} {
+		border-radius: 0;
+	}
 `;
 
 export const Mask = styled.div`
@@ -52,12 +90,30 @@ export const InnerFrame = styled.div`
 	column-gap: 3rem;
 	grid-template-rows: auto auto;
 	row-gap: 2.4rem;
+
+	@media ${devices.tablet600} {
+		height: 23.4rem;
+		width: 32.7rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+	}
 `;
 
 export const Map = styled.div`
 	grid-area: map;
 	border-radius: 1.5rem;
 	overflow: hidden;
+
+	@media ${devices.tablet} {
+		grid-row: 1 / span 1;
+		grid-column: 1 / span 1;
+	}
+
+	@media ${devices.tablet700} {
+		border-radius: 0;
+	}
 `;
 
 export const MapImage = styled.img`
@@ -69,11 +125,23 @@ export const MapImage = styled.img`
 export const Country = styled(Heading2)`
 	grid-column: 1 / span 2;
 	color: ${({ theme }) => theme.peach};
+
+	@media ${devices.tablet600} {
+		text-align: center;
+	}
 `;
 
-export const Address = styled.div``;
+export const Address = styled.div`
+	@media ${devices.tablet600} {
+		text-align: center;
+	}
+`;
 
-export const Contact = styled.div``;
+export const Contact = styled.div`
+	@media ${devices.tablet600} {
+		text-align: center;
+	}
+`;
 
 export const TextBold = styled(Body)`
 	font-size: 1.6rem;
