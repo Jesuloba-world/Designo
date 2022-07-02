7390e6498746365ff6ac61de12331e7564bd05ec
Jesuloba <jesulobajohn@gmail.com>
2022-07-02 15:50:20 +0100

    fix: change to browserrouter and using _redirects

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./globalStyles";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter>
 			<ThemeProvider theme={theme}>
 				<GlobalStyles />
 				<App />
 			</ThemeProvider>
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
